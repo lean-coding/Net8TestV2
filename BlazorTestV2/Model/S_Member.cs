546e5061fa0b152e3368b94d977ad973a1538076
azer <azerlee@gmail.com>
2024-07-24 10:53:35 +0800

    Blazor Exts (8)-註冊會員

---
+++ BlazorTestV2/Model/S_Member.cs
@@ -0,0 +1,38 @@
+﻿using Blazorise;
+
+namespace BlazorTestV2.Model
+{
+    public class S_Member
+    {
+        /// <summary>
+        /// 流水號
+        /// </summary>
+        public int SN { get; set; }
+
+        /// <summary>
+        /// 暱稱
+        /// </summary>
+        public string Nickname { get; set; }
+
+        /// <summary>
+        /// EMail
+        /// </summary>
+        public string EMail { get; set; }
+
+        /// <summary>
+        /// 密碼
+        /// </summary>
+        public string Password { get; set; }
+
+        /// <summary>
+        /// 狀態(A=Active, D=Dormant, S=Stop )
+        /// </summary>
+        public string Status { get; set; }
+
+        /// <summary>
+        /// 建立日期
+        /// </summary>
+        public DateTime CreateDate { get; set; }
+
+    }
+}
---
+++ BlazorTestV2/Service/Member.cs
@@ -0,0 +1,98 @@
+﻿using BlazorTestV2.Database;
+using BlazorTestV2.Model;
+using Dapper;
+
+namespace BlazorTestV2.Service
+{
+    public class Member
+    {
+        public bool Create(ref S_Member sMember)
+        {
+            try
+            {
+                string sql = @"
+INSERT INTO S_Member 
+(
+    Nickname,
+    EMail,
+    Password,
+    Status,
+    CreateDate
+) 
+VALUES 
+(
+    @Nickname,
+    @EMail,
+    @Password,
+    @Status,
+    datetime('now')
+);
+        
+SELECT last_insert_rowid(); ";
+
+                using (var conn = SQLiteHelper.dbConnection())
+                {
+                    sMember.SN = conn.QuerySingle<int>(sql, sMember);
+                    return true;
+                }
+            }
+            catch (Exception ex)
+            {
+
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// 更新短網址
+        /// </summary>
+        /// <param name="mURL"></param>
+        /// <returns></returns>
+        public bool Update(S_Member sMember)
+        {
+            try
+            {
+                string sql = @"
+UPDATE S_Member
+SET Nickname = @Nickname,
+    EMail = @EMail,
+    Password = @Password,
+    Status = @Status
+WHERE SN = @SN; ";
+
+                using (var conn = SQLiteHelper.dbConnection())
+                {
+                    conn.Execute(sql, sMember);
+                    return true;
+                }
+            }
+            catch (Exception ex)
+            {
+
+                return false;
+            }
+        }
+
+        public bool CheckEMailHasRegistered(string EMail)
+        {
+            try
+            {
+                string sql = @"
+SELECT * FROM S_Member
+WHERE EMail = @EMail; ";
+
+                using (var conn = SQLiteHelper.dbConnection())
+                {
+                    var results = conn.QueryFirst<S_Member>(sql, new { EMail = EMail });
+                    return (results != null);
+                }
+            }
+            catch (Exception ex)
+            {
+
+                return false;
+            }
+        }
+
+    }
+}
